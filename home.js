6ec26103c5879de75a5c6d241436d500c1d9ad33
aishaamunirr <aishaamunirr@gmail.com>
2025-09-11 15:57:32 +0100

    changes 3

--- home.js
+++ home.js
@@ -1,16 +1,16 @@
-const toggleBtn = document.getElementById("theme-toggle");
-const body = document.body;
-if (localStorage.getItem("theme") == "dark") {
-    body.classList.add("dark");
-    toggleBtn.textContent = "Light Mode";
-}
-toggleBtn.addEventListener("click", () =>{
-    body.classList.toggle("dark");
-    const darkModeon = body.classList.contains("dark");
+// const toggleBtn = document.getElementById("theme-toggle");
+// const body = document.body;
+// if (localStorage.getItem("theme") == "dark") {
+//     body.classList.add("dark");
+//     toggleBtn.textContent = "Light Mode";
+// }
+// toggleBtn.addEventListener("click", () =>{
+//     body.classList.toggle("dark");
+//     const darkModeon = body.classList.contains("dark");
 
-    toggleBtn.textContent = darkModeon ? "Light Mode" : "Dark Mode";
-    localStorage.setItem("theme", darkModeon ? "dark" : "light");
-});
+//     toggleBtn.textContent = darkModeon ? "Light Mode" : "Dark Mode";
+//     localStorage.setItem("theme", darkModeon ? "dark" : "light");
+// });
 
 // function toggleMenu() {
 //     const menu = document.getElementById('mobileMenu');
@@ -53,7 +53,7 @@ fetch("languages.json")
   .then(res => res.json())
   .then(data => {
     translations = data;
-    applyTranslations(); // Apply default language
+    applyTranslations();
   })
   .catch(err => console.error("Failed to load translations:", err));
 
@@ -62,7 +62,7 @@ function changeLanguage() {
   const desktopSelect = document.getElementById("languageSelect");
   const mobileSelect = document.getElementById("languageSelect2");
   currentLang = desktopSelect.value;
-  mobileSelect.value = currentLang; // sync mobile
+  mobileSelect.value = currentLang; 
   applyTranslations();
 }
 
